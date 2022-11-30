fb1e23232bffdefa68dba92cf6d4cb7c1e7b160f
Sup3r-Us3r <maydersonmello@gmail.com>
2022-11-30 19:45:02 -0300

    create secure connection between server and client

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -2,20 +2,72 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/Sup3r-Us3r/go-grpc/grpc/pb"
 	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
-func createProduct() {
-	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	rootDirectory, err := os.Getwd()
 
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
+	}
+
+	// Load certificate of the CA who signed server's certificate
+	pemServerCA, err := ioutil.ReadFile(
+		filepath.Join(rootDirectory, "grpc", "cert", "ca-cert.pem"),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+
+	// Load client's certificate and private key
+	clientCert, err := tls.LoadX509KeyPair(
+		filepath.Join(rootDirectory, "grpc", "cert", "client-cert.pem"),
+		filepath.Join(rootDirectory, "grpc", "cert", "client-key.pem"),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
+func createProduct() {
+	tlsCredentials, err := loadTLSCredentials()
+
+	if err != nil {
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+
+	connection, err := grpc.Dial("0.0.0.0:50051", grpc.WithTransportCredentials(tlsCredentials))
+
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
 	}
 
 	client := pb.NewProductServiceClient(connection)
@@ -32,11 +84,39 @@ func createProduct() {
 	log.Print(response)
 }
 
+// gRPC client without security
+// func createProduct() {
+// 	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	client := pb.NewProductServiceClient(connection)
+
+// 	request := &pb.CreateProductRequest{
+// 		Name: "Product example",
+// 	}
+// 	response, err := client.CreateProduct(context.Background(), request)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Print(response)
+// }
+
 func listProducts() {
-	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tlsCredentials, err := loadTLSCredentials()
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+
+	connection, err := grpc.Dial("0.0.0.0:50051", grpc.WithTransportCredentials(tlsCredentials))
+
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
 	}
 
 	client := pb.NewProductServiceClient(connection)
@@ -50,6 +130,26 @@ func listProducts() {
 
 	log.Print(response)
 }
+
+// gRPC client without security
+// func listProducts() {
+// 	connection, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	client := pb.NewProductServiceClient(connection)
+
+// 	request := &empty.Empty{}
+// 	response, err := client.ListProducts(context.Background(), request)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	log.Print(response)
+// }
 
 func main() {
 	listProducts()
--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,26 +1,80 @@
 package server
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net"
+	"os"
+	"path/filepath"
 
 	"github.com/Sup3r-Us3r/go-grpc/grpc/pb"
 	"github.com/Sup3r-Us3r/go-grpc/grpc/service"
 	"github.com/Sup3r-Us3r/go-grpc/model"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/reflection"
 )
 
 var ProductList = model.NewProducts()
 
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	rootDirectory, err := os.Getwd()
+
+	if err != nil {
+		return nil, err
+	}
+
+	pemClientCA, err := ioutil.ReadFile(
+		filepath.Join(rootDirectory, "grpc", "cert", "ca-cert.pem"),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+
+	if !certPool.AppendCertsFromPEM(pemClientCA) {
+		return nil, fmt.Errorf("failed to add client CA's certificate")
+	}
+
+	// Load server's certificate and private key
+	serverCert, err := tls.LoadX509KeyPair(
+		filepath.Join(rootDirectory, "grpc", "cert", "server-cert.pem"),
+		filepath.Join(rootDirectory, "grpc", "cert", "server-key.pem"),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the credentials and return it
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func SetupGrpcServer() {
-	listener, err := net.Listen("tcp", "localhost:50051")
+	tlsCredentials, err := loadTLSCredentials()
+
+	if err != nil {
+		log.Fatal("cannot load TLS credentials: ", err)
+	}
+
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
 
 	if err != nil {
 		panic(err)
 	}
 
-	grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer(grpc.Creds(tlsCredentials))
 	reflection.Register(grpcServer)
 
 	productService := service.NewProductGrpcService()
@@ -31,3 +85,23 @@ func SetupGrpcServer() {
 
 	grpcServer.Serve(listener)
 }
+
+// gRPC server without security
+// func SetupGrpcServer() {
+// 	listener, err := net.Listen("tcp", "localhost:50051")
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	grpcServer := grpc.NewServer()
+// 	reflection.Register(grpcServer)
+
+// 	productService := service.NewProductGrpcService()
+// 	productService.Products = ProductList
+// 	pb.RegisterProductServiceServer(grpcServer, productService)
+
+// 	fmt.Println("GRPC IS RUNNING ON PORT 50051")
+
+// 	grpcServer.Serve(listener)
+// }
